70fcab4a46171ba2ea65f95d7fa9656a0b0db919
satyaxo <dummy@gmail.com>
2026-02-21 10:37:47 +0000

    Day 1 task

--- server/src/main/java/com/edutech/progressive/entity/Cricketer.java
+++ server/src/main/java/com/edutech/progressive/entity/Cricketer.java
@@ -1,5 +1,97 @@
 package com.edutech.progressive.entity;
 
 public class Cricketer {
+    private int cricketerId;
+    private int teamId;
+    private String cricketername;
+    private int age;
+    private String nationality;
+    private int experience;
+    private String role;
+    private int totalRuns;
+    private int totalWickets;
+
+    public Cricketer() {
+    }
+
+    public int getCricketerId() {
+        return cricketerId;
+    }
+
+    public void setCricketerId(int cricketerId) {
+        this.cricketerId = cricketerId;
+    }
+
+    public int getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(int teamId) {
+        this.teamId = teamId;
+    }
+
+    public String getCricketername() {
+        return cricketername;
+    }
+
+    public void setCricketername(String cricketername) {
+        this.cricketername = cricketername;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getTotalRuns() {
+        return totalRuns;
+    }
+
+    public void setTotalRuns(int totalRuns) {
+        this.totalRuns = totalRuns;
+    }
+
+    public int getTotalWickets() {
+        return totalWickets;
+    }
+
+    public void setTotalWickets(int totalWickets) {
+        this.totalWickets = totalWickets;
+    }
+
+    
+
+
+
+    
+
+
 
 }
