8a29fe4e43d4e03171516cc4c79fcf8595501249
Tejas Zope <148805291+Tejas499@users.noreply.github.com>
2026-03-24 19:34:37 +0530

    Delete server/src directory

--- server/src/main/java/com/edutech/progressive/IplApplication.java
+++
@@ -1,7 +0,0 @@
-package com.edutech.progressive;
-
-public class IplApplication {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Ipl Progressive Project!");
-    }
-}
--- server/src/main/java/com/edutech/progressive/dao/MatchDAOImpl.java
+++
@@ -1,5 +0,0 @@
-package com.edutech.progressive.dao;
-
-public class MatchDAOImpl {
-
-}
